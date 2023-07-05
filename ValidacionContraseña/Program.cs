2fa35a4dae4ea61ec16f8bc3d92a3d6deb65f267
Pierre <Pierre@DESKTOP-UJADA9E>
2023-07-04 22:42:39 -0500

    Agregar archivos de proyecto.

---
+++ ValidacionContraseña/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionContraseña
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string contraseña1, contraseña2 = "";
+
+            Console.Write("Escribe tu contraseña:  ");
+            contraseña1 = Console.ReadLine();
+            while (contraseña1 != contraseña2)
+            {
+                Console.Write("Confirma tu contraseña:  ");
+                contraseña2 = Console.ReadLine();
+            } 
+            Console.WriteLine("Contraseña exitosa");
+
+
+        }
+    }
+}
